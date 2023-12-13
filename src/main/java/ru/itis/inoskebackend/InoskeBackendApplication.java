d34f49ab2d8b7445dabffbd9fff35bf67840f0dc
Vladislav Nazarychev <vnazarychev@fix.ru>
2023-12-13 20:43:32 +0300

    add docker compose and first structure

---
+++ src/main/java/ru/itis/inoskebackend/InoskeBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.inoskebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InoskeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InoskeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itis/inoskebackend/InoskeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.inoskebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InoskeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
